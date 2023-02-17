c132cba514a9a058c6244d805bc9d6985cfe23d5
Nurudeen <rainojo@gmail.com>
2023-02-17 15:49:23 -0800

    my first c programm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * maim - put a code
- * Return: Always 0
+ * main - Entry
+ * Return: Always
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+print("\"Programming is like building a multilingual puzzle");
+return (0);
 }
