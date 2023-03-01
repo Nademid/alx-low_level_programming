d5e7c752e2f70e45fd2e077b3fe15c867cbd1fab
Nurudeen <rainojo@gmail.com>
2023-03-01 01:02:40 -0800

    zero

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * _strcat - concat 2 string
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
