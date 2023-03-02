82eab496646b13644d2aa7af96bd6614c8cdda00
Nurudeen <rainojo@gmail.com>
2023-03-02 00:53:27 -0800

    zero

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,21 +6,23 @@ include "main.h"
  * @src: char
  * Return: char
  */
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
+	int i;
+	int j;
 
-	while (*dest != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest++;
+		i++;
 	}
-
-	while (*src != '\0')
+	j = 0;
+	while (src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	*dest = '\0';
-	return (s);
+	dest[i] = '\0';
+	return (dest);
 }
