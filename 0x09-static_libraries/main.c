3bc7ab1dc12b160a77ec38e2890ee752a455429a
Nurudeen <rainojo@gmail.com>
2023-03-13 16:02:14 -0700

    main c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	 char *("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	 _put("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return(0);
 }
